6cf39dab8f73f6b33c1dc1101a1080f848e974a3
anil babu <anilb9850@gmail.com>
2022-10-09 16:13:54 +0530

    fixed bug in steps

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("fbbbb")
+console.log("fbbbb");
 const box1 = document.querySelector(".content1a");
 const box2 = document.querySelector(".content1b");
 const box3 = document.querySelector(".content1c");
@@ -11,50 +11,80 @@ const stepcounter4 = document.querySelector("#step4");
 const stepcounter5 = document.querySelector("#step5");
 
 let count = "a";
-
+let get = false;
 box1.addEventListener("click", () => {
-    stepcounter1.style.background = "green";
-    count = "b";
-})
+  stepcounter1.style.background = "green";
+  count = "b";
+  localStorage.setItem("b", true);
+});
 
 box2.addEventListener("click", () => {
-
-    if (count != "b") {
-        console.log("attempt level 1 first");
-    }
-    else {
-        stepcounter2.style.background = "green";
+  if (count != "b") {
+    console.log("attempt level 1 first");
+  } else {
+    stepcounter2.style.background = "green";
     count = "c";
-    }
-    
-})
+    localStorage.setItem("c", true);
+  }
+});
 
 box3.addEventListener("click", () => {
-
-    if (count != "c") {
-        console.log("attempt level 1 first");
-    }
-    else {
-        stepcounter3.style.background = "green";
-        count ="d";
-    }
-})
+  if (count != "c") {
+    console.log("attempt level 1 first");
+  } else {
+    stepcounter3.style.background = "green";
+    count = "d";
+    localStorage.setItem("d", true);
+  }
+});
 box4.addEventListener("click", () => {
-    if (count != "d") {
-        console.log("attempt level 1 first");
-    }
-    else {
-        stepcounter4.style.background = "green";
-        count = "e";
-    }
-})
+  if (count != "d") {
+    console.log("attempt level 1 first");
+  } else {
+    stepcounter4.style.background = "green";
+    count = "e";
+    localStorage.setItem("e", true);
+  }
+});
 box5.addEventListener("click", () => {
+  if (count != "e") {
+    console.log("attempt level 1 first");
+  } else {
+    stepcounter5.style.background = "green";
+    count = "f";
+    localStorage.setItem("f", true);
+    get = true;
+  }
+});
 
-    if (count != "e") {
-        console.log("attempt level 1 first");
-    }
-    else {
-        stepcounter5.style.background = "green";
-        count = "f";
-    }
-})
+const b = localStorage.getItem("b");
+
+if (b) {
+  stepcounter1.style.background = "green";
+}
+
+const c = localStorage.getItem("c");
+
+if (c) {
+  stepcounter2.style.background = "green";
+}
+
+const d = localStorage.getItem("d");
+
+if (d) {
+  stepcounter3.style.background = "green";
+}
+
+const e = localStorage.getItem("e");
+
+if (e) {
+  stepcounter4.style.background = "green";
+}
+
+const f = localStorage.getItem("f");
+
+if (f) {
+  stepcounter5.style.background = "green";
+}
+
+console.log("the values of", a);
